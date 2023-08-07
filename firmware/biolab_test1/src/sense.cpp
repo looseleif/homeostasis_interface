b323757982bc5c9027b5402565f50247c2580093
Chase <chase.anderson.000@gmail.com>
2023-08-06 22:18:35 -0500

    sense driver

--- firmware/biolab_test1/src/sense.cpp
+++ firmware/biolab_test1/src/sense.cpp
@@ -7,15 +7,13 @@ sense::sense(const uint8_t port, menu *menuptr, oled *oledptr, strip *stripptr){
 
   sense_soft->i2c_init();
 
-  // write your own god damn driver chase!
-  // after swimming that, gotta clear the mind
+  if (!sense_soft->i2c_start(ADXL343_ADDRESS<<1| I2C_WRITE | (ADXL3XX_REG_DATAX1<<8|ADXL3XX_REG_DATAX0&0x10000 ? 8 : 0))) return;
 
-  // _sensorID = sensorID;
-  // _wire = &Wire;
-  // _addr = addr;
-  // _wire = theWire;
-  // _begun = false;
+  // send the address
+  if (!sense_soft->i2c_write(ADXL3XX_REG_BW_RATE&0xFF)) return;
 
+
+  delay(6);
   // sense_accel = new Adafruit_ADXL343(12345);
   // sense_accel->setRange(ADXL343_RANGE_2_G);
   // sense_accel->setDataRate(ADXL343_DATARATE_1600_HZ);
@@ -47,11 +45,12 @@ void sense::updateGame(int x)
   if (!sense_soft->i2c_write(ADXL3XX_REG_DATAX1&0xFF)) return;
   if (!sense_soft->i2c_write(ADXL3XX_REG_DATAX0&0xFF)) return;
 
-
-  // read
+  // read X
   _X = 0x0000;
   _X &= (sense_soft->i2c_read(true))<<8;
   _X &= (sense_soft->i2c_read(true));
+
+  delay(6);
 
   if (!sense_soft->i2c_rep_start(ADXL343_ADDRESS<<1| I2C_READ | (ADXL3XX_REG_DATAY1<<8|ADXL3XX_REG_DATAY0&0x10000 ? 8 : 0))) return;
 
@@ -59,25 +58,29 @@ void sense::updateGame(int x)
   if (!sense_soft->i2c_write(ADXL3XX_REG_DATAY1&0xFF)) return;
   if (!sense_soft->i2c_write(ADXL3XX_REG_DATAY0&0xFF)) return;
 
-
-  // read
+  // read Y
   _Y = 0x0000;
   _Y &= (sense_soft->i2c_read(true))<<8;
   _Y &= (sense_soft->i2c_read(true));
 
-    if (!sense_soft->i2c_rep_start(ADXL343_ADDRESS<<1| I2C_READ | (ADXL3XX_REG_DATAZ1<<8|ADXL3XX_REG_DATAZ0&0x10000 ? 8 : 0))) return;
+  delay(6);
+
+  if (!sense_soft->i2c_rep_start(ADXL343_ADDRESS<<1| I2C_READ | (ADXL3XX_REG_DATAZ1<<8|ADXL3XX_REG_DATAZ0&0x10000 ? 8 : 0))) return;
 
   // send the address
   if (!sense_soft->i2c_write(ADXL3XX_REG_DATAZ1&0xFF)) return;
   if (!sense_soft->i2c_write(ADXL3XX_REG_DATAZ0&0xFF)) return;
 
-
-  // read
+  // read Z
   _Z = 0x0000;
   _Z &= (sense_soft->i2c_read(true))<<8;
   _Z &= (sense_soft->i2c_read(true));
 
   delay(6);
+
+  sense_soft->i2c_stop();
+
+
 
   return;
 }
